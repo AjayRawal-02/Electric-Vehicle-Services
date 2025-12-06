5e56336eb85f759621e8f7c98c988695acdb6e80
Satendra Baghel <143244442+satendra27@users.noreply.github.com>
2025-12-06 14:06:32 +0530

    Update MyBookings.jsx

--- Frontend/src/Components/MyBookings.jsx
+++ Frontend/src/Components/MyBookings.jsx
@@ -16,7 +16,7 @@ const MyBookings = () => {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/bookings/my-bookings", {
+      const res = await fetch("https://electric-vehicle-services.onrender.com/api/bookings/my-bookings", {
         headers: { Authorization: `Bearer ${token}` },
       });
 
