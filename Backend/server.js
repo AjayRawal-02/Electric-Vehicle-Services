1258f1f5baf08f1851051a40b7031692013edc88
Satendra Baghel <143244442+satendra27@users.noreply.github.com>
2025-12-06 09:36:33 +0530

    Added

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,8 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../Frontend/dist");
   app.use(express.static(frontendPath));
-  app.get("*", (req, res) => {
+
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
